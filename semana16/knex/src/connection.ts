32c6c61bea3943ec580b84fa4b8efa1b099c1072
Drenovaa <guilherme_silva_123@hotmail.com>
2021-07-21 15:30:59 -0300

    Aula 47 Knex

--- semana16/knex/src/connection.ts
+++ semana16/knex/src/connection.ts
@@ -1,12 +1,14 @@
 import knex from 'knex'
+import dotenv from 'dotenv'
+dotenv.config()
 const connection = knex({ // Estabelece conexão com o banco
  client: "mysql",
  connection: {
- host: "35.226.146.116",
+ host: process.env.DB_HOST,
  port: 3306,
- user: "2125554-guilherme-rodrigues",
- password: "wEBIZVvywxQXZ9V3hvo0",
- database: "paiva-2125554-guilherme-rodrigues",
+ user: process.env.DB_USER,
+ password: process.env.DB_PASSWORD,
+ database: process.env.DB_SCHEMA,
  multipleStatements: true
  }
 })
